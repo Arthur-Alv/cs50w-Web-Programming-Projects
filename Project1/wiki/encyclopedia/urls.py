fd68c40cf6eefa1f23954bfa78e7d34186427e6a
Arthur Alves Magalhães <arthur.alves@transfacil.com.br>
2024-07-17 16:50:32 -0300

    updating again project1

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.result, name="result")
+    path("wiki/<str:title>", views.result, name="result"),
+    path("search/", views.search, name="search"),
+    path("new/", views.newPage, name="newPage")
 ]
--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -1,8 +1,12 @@
+from django import forms
 from django.shortcuts import render
 import markdown
 
 from . import util
 
+
+class NewPage(forms.Form):
+    task = forms.CharField(label="New Task")
 
 def convert_md_to_html(title):
     content = util.get_entry(title)
@@ -33,4 +37,28 @@ def result(request, title):
             "result": converting
         })
 
+def search(request):
+    if request.method == "POST":
+        entry_send = request.POST['q']
+        converting = convert_md_to_html(entry_send)
+        if converting is not None:
+            return render(request, "encyclopedia/result.html", {
+                "title": entry_send,
+                "result": converting
+            })
+        else:
+            allEntries = util.list_entries()
+            recommendation = []
+            for entry in allEntries:
+                if entry_send.lower() in entry.lower():
+                    recommendation.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "recommendation": recommendation
+            })
 
+def newPage(request):
+    if request.method == 'GET':
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
